ca8017c79f4b8a894e4053993ef93c35473038e4
OliBoucher04 <oli_boucher@outlook.com>
2024-03-04 16:24:13 -0500

    feature-config-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          tealPerso: {
+            100: "#2394AD",
+            200: "#1D8590",
+            300: "#19687A"
+          },
+          grayPerso: {
+            100: "#5E6565",
+            200: "#363F3F",
+            300: "#1E2323",
+            400: "#090A0A",
+          } 
+        },
+        screens: {
+          'xs': '300px',
+        },
+      },
     },
     plugins: [],
   }
