3330f5160e736c03c8c09d6a8a4e99ec930dc6db
OliBoucher04 <oli_boucher@outlook.com>
2024-03-06 16:34:14 -0500

    feature-mise-en-page-menu

--- src/components/BarreMenu.jsx
+++ src/components/BarreMenu.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import services from "../Data/services.json";
-import { logoHeader } from "../assets";
+import { logoHeader, strata } from "../assets";
 import { IoIosMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+import { MdArrowDropDown } from "react-icons/md";
+
 
 const BarreMenu = () => {
   const [servicesOpen, setServicesOpen] = useState(false);
@@ -37,7 +40,9 @@ const BarreMenu = () => {
   return (
     <section className={`fixed top-0 flex flex-row justify-center items-center w-full z-[100] px-3 md:px-6 py-4 xl:w-[100%] ${mobileMenuOpen ? "mobile-menu-open" : ""} ${scrolled ? 'bg-grayPerso-300 duration-300 ease-linear' : 'bg-transparent'}`}>
       <div className="flex w-[90%] justify-between items-center md:w-[95%] lg:w-[90%]">
-        <img src={logoHeader} alt="" className="w-[190px] mr-8"/>
+        <img src={logoHeader} alt="" className="w-[190px] z-[100] mr-8" />
+
+        {/* MENU DESKTOP */}
         <div className="md:w-full">
           <div className="hidden md:flex text-white flex-row justify-between items-center">
             <nav>
@@ -84,29 +89,36 @@ const BarreMenu = () => {
               </ul>
             </nav>
           </div>
-          <div className="md:hidden">
-            <div className="hamburger-menu text-4xl text-white" onClick={toggleMobileMenu}>
+
+          {/* MENU MOBILE */}
+          <div className="md:hidden text-white">
+            <div className="hamburger-menu text-4xl text-white z-[100]" onClick={toggleMobileMenu}>
               <IoIosMenu />
             </div>
             {mobileMenuOpen && (
-              <ul>
-                <li>
-                  <a href="#" onClick={() => setServicesOpen(!servicesOpen)}>
+              <ul className="absolute bg-grayPerso-300 w-full h-screen left-0 top-0 flex flex-col justify-start pt-72 items-center">
+                <img src={strata} alt="strata" className="absolute top-20 h-[100vw] sm:h-[80vw] object-cover" />
+                <IoClose className="absolute top-5 right-5 text-4xl cursor-pointer" onClick={toggleMobileMenu} />
+                <li className="z-[200] mb-6 text-center w-[80%]">
+                  <a href="#" className="catMenu" onClick={() => setServicesOpen(!servicesOpen)}>
                     Services
                   </a>
                   {servicesOpen && (
-                    <nav>
-                      <ul>
+                    <nav className="mt-4 mb-4 w-[100%]">
+                      <ul className="text-center">
                         {services.map((category, index) => (
-                          <li key={index}>
-                            <a href="#" onClick={() => toggleSousCat(index)}>
-                              {category.category}
-                            </a>
+                          <li key={index} className="w-full flex flex-col justify-between items-start">
+                            <div className="w-full flex justify-between items-center">
+                              <a href="#" className="sousCatMenu" onClick={() => toggleSousCat(index)}>
+                                {category.category}
+                              </a>
+                              <MdArrowDropDown />
+                            </div>
                             {currentSousCatIndex === index && (
-                              <ul>
+                              <ul className="mt-2 mb-4 flex flex-col justify-start items-start w-full">
                                 {category.services.map(
                                   (service, servicesIndex) => (
-                                    <li key={servicesIndex}>
+                                    <li key={servicesIndex} className="text-left">
                                       <a href="#">{service.nom}</a>
                                     </li>
                                   )
@@ -119,16 +131,16 @@ const BarreMenu = () => {
                     </nav>
                   )}
                 </li>
-                <li>
+                <li className="catMenu">
                   <a href="#">Carrières</a>
                 </li>
-                <li>
+                <li className="catMenu">
                   <a href="#">À propos</a>
                 </li>
-                <li>
+                <li className="catMenu">
                   <a href="#">Réalisations</a>
                 </li>
-                <li>
+                <li className="catMenu">
                   <a href="#">Nous joindre</a>
                 </li>
               </ul>
